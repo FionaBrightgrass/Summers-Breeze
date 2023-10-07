1bc08fd02fc845202245fa0ed668329ae8a479a4
Fiona Brightgrass <lvl99fiona@gmail.com>
2023-10-07 15:41:05 -0400

    test3

--- scripts/summersbreeze.js
+++ scripts/summersbreeze.js
@@ -38,15 +38,5 @@ Hooks.on('combatTurn', async (combat, update, _options) => {
 });
 
 Hooks.on('pf1ToggleActorBuff',  async(actor, itemData) =>{
-    if(Utils.shouldHandle() && itemData.getItemDictionaryFlag('radius') > 0){
-        if(sceneTokens?.length < 1){
-            sceneTokens.length = 0;
-            sceneTokens = Utils.createTokenArray();
-        }
-        let tokens = actor.getActiveTokens();
-        if(tokens?.length > 0){
-            let token = tokens[0].document;
-            breezelogic.refreshAuras(token, sceneTokens, false);
-        }
-    }
+    console.log("Fucking do shit");
 })
